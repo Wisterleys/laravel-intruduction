676da4c3d5149a9d17d2b95d22c8a90dc6b1f4b8
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-04-22 11:54:43 -0300

    Testando e criando rotas

--- primeiroprojeto/routes/web.php
+++ primeiroprojeto/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Route::get('usuario/{id}', function ($id) {
+    return "usuário ".$id;
+}); */
+
+Route::get('usuario/{id?}', function ($id=null) {
+    return "usuário ".$id;
+});
